b21cc0de5932d9b5bb7221eb88412b0cb1188038
PiXeleum <jeffreyrigaux@gmail.com>
2022-04-07 11:33:27 +0200

    finish

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
 // First Labour : Heracles vs Nemean Lion
-// use your Figher class here
+// use your Fighter class here
+const Fighter = require('./src/Fighter');
+
+  const fighters = [
+      new Fighter (
+        "🧔 Heracles",
+        20,
+        6,
+    ),
+
+      new Fighter (
+        "🦁 Nemean Lion",
+        11,
+        13,
+    ),
+  ];
+
+  let heracles = fighters[0];
+  let lion = fighters[1];
+
+  for(let i = 1; heracles.life > 0 && lion.life > 0; i++) {
+        console.log(`Round ${i}`);
+        heracles.fight(lion);
+        console.log(`${heracles.name} fight ${lion.name} the pv of ${lion.name} is ${lion.life}`);
+        lion.fight(heracles);
+        console.log(`${lion.name} fight ${heracles.name} the pv of ${heracles.name} is ${heracles.life}`);
+  }
+
+  if (heracles.life ===0) {
+      console.log(`winner is ${lion.name} 👑`)
+      console.log(`${heracles.name} is dead 💀`)
+  } else {
+    console.log(`winner is ${heracles.name} 👑`)
+    console.log(`${lion.name} is dead 💀`)
+  }
+
+
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,26 @@
 /* Fighter class definition */
+const Max_life = 100;
+class Fighter {
+    constructor(name, strenght, dexterity,  life = Max_life) {
+    this.name = name;
+    this.strenght = strenght; 
+    this.dexterity = dexterity;
+    this.life = life;
+    }
+
+    fight (oppenent) {
+        let damage = Math.floor(Math.random() * this.strenght) - oppenent.dexterity;
+        if(damage < 0){
+            damage = 0;
+        }
+    
+        oppenent.life = oppenent.life - damage;
+        if(oppenent.life < 0){
+            oppenent.life = 0;
+        }
+
+    }
+};
+
+
+module.exports = Fighter;
